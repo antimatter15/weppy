304a876bda1c3d25a9a0535e0366263f645614b8
antimatter15 <antimatter15@gmail.com>
2010-10-03 15:16:57 +0000

    works

--- webp.js
+++ webp.js
@@ -1,5 +1,5 @@
-var webm_template = "\x1aE\xdf\xa3\x40\x20B\x86\x81\x01B\xf7\x81\x01B\xf2\x81\x04B\xf3\x81\x08B\x82\x40\x04webmB\x87\x81\x02B\x85\x81\x02\x18S\x80g\x40e\x16T\xaek\x40I\xae\x40F\xd7\x81\x01c\xc5\x81\x01\x9c\x81\x00\x22\xb5\x9c\x40\x03und\x86\x40\x05V_VP8\x83\x81\x01\x23\xe3\x83\x40\x04\x02bZ\x00\xe0\x40\x1d\xb0\x40\x0bPIXEL_WIDTH\xba\x40\x0cPIXEL_HEIGHT\x1fC\xb6u\x40\x10\xe7\x81\x00\xa3\x40\x0aIMAGE_DATA";
-var image_header = '\x81\x00\x00\x80';
+//toDataURL(toWebM(parseWebP(parseRIFF(
+
 
 function parseRIFF(string){
   var offset = 0;
@@ -28,6 +28,19 @@ function parseRIFF(string){
 
 
 
+function toBinStr(bits){
+  var data = '';
+  var pad = (bits.length % 8) ? (new Array(1 + 8 - (bits.length % 8))).join('0') : '';
+  bits = pad + bits;
+  for(var i = 0; i < bits.length; i+= 8){
+    data += String.fromCharCode(parseInt(bits.substr(i,8),2))
+  }
+  return data;
+}
+
+
+
+
 function encode_64(input) {
 	var output = "", i = 0, l = input.length,
 	key = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=", 
@@ -47,9 +60,18 @@ function encode_64(input) {
 	return output;
 }
 
+function numToBin(number){
+  var bits = number.toString('2');
+  var data = '';
+  var pad = (bits.length % 8) ? (new Array(1 + 8 - (bits.length % 8))).join('0') : '';
+  bits = pad + bits;
+  for(var i = 0; i < bits.length; i+= 8){
+    data += String.fromCharCode(parseInt(bits.substr(i,8),2))
+  }
+  return data;
+}
 
-function parseWebP(string){
-  var riff = parseRIFF(string);
+function parseWebP(riff){
   var VP8 = riff.RIFF[0].WEBP[0];
 
   var frame_start = VP8.indexOf('\x9d\x01\x2a')
@@ -74,12 +96,78 @@ function parseWebP(string){
 }
 
 
+function generateEBML(json){
+  var ebml = '';
+  for(var i in json){
+    var el_len = json[i].pop ? json[i].length : 1;
+    for(var k = 0; k < el_len; k++){
+      var data = json[i].pop ? json[i][k] : json[i];
+      console.log(schema[i].name, i,k,data);
+      var hexid = i;
+      if(typeof data == 'object'){
+        data = generateEBML(data);
+      }else if(typeof data == 'number'){
+        data = toBinStr(data.toString(2));
+      }
+      if(typeof data == 'string'){
+        var len = data.length;
+        var zeroes = Math.ceil(Math.ceil(Math.log(len)/Math.log(2))/8);
+        //(zeroes + 1) * 8 - (zeroes + 1) = zeroes * 7 - 7 = needed size
+        var size_str = len.toString(2);
+        var padded = (new Array((zeroes * 7 + 7 + 1) - size_str.length)).join('0') + size_str;
+        var size = (new Array(zeroes + 1)).join('0') + '1' + padded;
+        ebml += toBinStr(parseInt(hexid, 16).toString(2));
+        ebml += toBinStr(size);
+        ebml += data;
+      }else{
+        console.log('big error!?!?!?',i,data,json);
+      }
+    }
+  }
+  return ebml;
+}
+
+
+
 function toWebM(image){
-  var webm = webm_template
-    .replace('PIXEL_HEIGHT', image.height)
-    .replace('PIXEL_WIDTH', image.width)
-    .replace('IMAGE_DATA', '\x81\x00\x00\x80' + image.data.substr(4));
-  return webm;
+
+  //converted using JSON.stringify(EBML,null,'  ').replace(/\w+/g,function(b){return nameHexMap[b]||b})
+  //the EBML header is not present because of sorting issues, it doesn't always end up the first
+  var EBML = {
+    "18538067": [
+      {
+        "1654ae6b": [
+          {
+            "ae": [
+              {
+                "d7": 1,
+                "63c5": 1,
+                "9c": 0,
+                "22b59c": "und",
+                "86": "V_VP8",
+                "83": 1,
+                "23e383": 40000000,
+                "e0": {
+                  "b0": image.width,
+                  "ba": image.height
+                }
+              }
+            ]
+          }
+        ],
+        "1f43b675": [
+          {
+            "e7": 0,
+            "a3": [
+              '\x81\x00\x00\x80' + image.data.substr(4)
+            ]
+          }
+        ]
+      }
+    ]
+  };
+
+  return "\x1aE\xdf\xa3\x40\x20B\x82\x40\x04webmB\x85\x81\x02B\x86\x81\x01B\x87\x81\x02B\xf7\x81\x01B\xf2\x81\x04B\xf3\x81\x08" + generateEBML(EBML);
 }
 
 function toDataURL(webm){
